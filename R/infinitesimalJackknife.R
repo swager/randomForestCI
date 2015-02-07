a6cde4ec5cacb1e6fb684c2eea5b7986b6449113
Stefan Wager <swager@stanford.edu>
2015-02-06 19:49:20 -0800

    infjack

--- R/infinitesimalJackknife.R
+++ R/infinitesimalJackknife.R
@@ -1,34 +1,37 @@
-#' The infinitesimal jackknife for random forests with subsampling
+#' The infinitesimal jackknife for random forests
 #'
 #' @param rf A random forest trained with replace = TRUE and keep.inbag = TRUE
 #' @param newdata A set of test points at which to evaluate standard errors
 
-randomForestInfJackSubsample = function(rf, newdata) {
+randomForestInfJack = function(rf, newdata) {
 	
 	if (is.null(rf$inbag)) {
 		stop("Random forest must be trained with keep.inbag = TRUE")
+	}
+	
+	if (length(levels(factor(colSums(rf$inbag)))) > 1) {
+		stop("The keep.inbag field must store the number of times each observation was used")
 	}
 	
 	B = rf$ntree
 	n = length(rf$y)
 	s = sum(rf$inbag) / B
 	
-	obs.seen = sum(rf$inbag) + sum(rf$oob.times)
-	if (obs.seen != B * n | s %% 1 != 0) {
-		stop("Random forest must be trained with subsampling (i.e., replace = FALSE)")
-	}
-	
 	predictions = predict(rf, newdata, predict.all = TRUE)
-	agg_pred = predictions$aggregate
+	y.hat = predictions$aggregate
 	pred = predictions$individual
 	pred.centered = pred - rowMeans(pred)
 	
 	N = Matrix(rf$inbag, sparse = TRUE)
+	N.avg = rowMeans(N)
 	
-	raw.IJ = colSums((N %*% t(pred.centered) - s/n * Matrix(1, nrow(N), 1) %*% Matrix(colSums(pred.centered), 1, nrow(pred.centered)))^2) / B^2
+	C = N %*% t(pred.centered) - Matrix(N.avg, nrow(N), 1) %*% Matrix(rowSums(pred.centered), 1, nrow(pred.centered))	
+	raw.IJ = colSums(C^2) / B^2
+	
+	N.var = mean(rowMeans(N^2) - rowMeans(N)^2)
 	boot.var = rowSums(pred.centered^2) / B
-	bias.correction = s * (n - s) / n * boot.var / B
+	bias.correction = n * N.var * boot.var / B
 	vars = raw.IJ - bias.correction;
 
-	data.frame(y.hat=agg_pred, var.hat=vars);
+	data.frame(y.hat=y.hat, var.hat=vars);
 }
